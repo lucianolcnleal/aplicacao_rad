0370b9285d25089df6a01387b81c85b11422561e
lucianolcnleal <luciano.leal@ifmt.edu.br>
2024-11-30 18:27:53 -0300

    Adiciona funcao multiplica

--- calculadora.py
+++ calculadora.py
@@ -3,3 +3,6 @@ def soma(x,y):
 
 def subtrair(x,y):
     return x-y
+
+def multiplica(x,y):
+    return x*y
--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 import calculadora
 print(calculadora.soma(5,5))
+print(calculadora.subtrair(5,5))
+print(calculadora.multiplica(5,5))
